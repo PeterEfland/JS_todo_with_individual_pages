19bfc49648efb592f607ded3c95e60772fd82394
Peter <phefland@gmail.com>
2019-03-13 17:44:23 -0300

    re-factor  and styling of app

--- scripts/todo-app.js
+++ scripts/todo-app.js
@@ -9,29 +9,30 @@ const filters = {
 
 renderTodos (todos, filters)
 
-document.querySelector("#search-text").addEventListener("input", (e) => {
+document.querySelector('#search-text').addEventListener('input', (e) => {
     filters.searchText = e.target.value 
-    renderTodos (todos, filters)
+    renderTodos(todos, filters)
 })
-
 
 document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault()
-    todos.push({
+  const text = e.target.elements.text.value.trim();
+  e.preventDefault();
+
+  if (text.length > 0) {
+      todos.push({
         id: uuidv4(),
-        text: e.target.elements.newTodo.value,
+        text,
         completed: false
     })
-    saveTodos(todos)
-    renderTodos(todos, filters)
-    e.target.elements.newTodo.value = ''
-})
-        
+    saveTodos(todos);
+    renderTodos(todos, filters);
+    e.target.elements.text.value = ""
+  }
+}) 
+
 
 document.querySelector('#hide-completed').addEventListener('change', (e) => {
     filters.hideCompleted = e.target.checked    
     renderTodos (todos, filters)     
 })
 
-
-   
